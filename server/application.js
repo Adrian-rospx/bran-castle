3b1eb2bbe31bc896e61bec2936fa87b3fc0cefc2
Adrian-rospx <adrianrosu5400@gmail.com>
2025-10-30 20:50:41 +0200

    added: basic on/off logging

--- server/application.js
+++ server/application.js
@@ -1,4 +1,34 @@
+import express from "express";
 import dotenv from "dotenv";
+
+import prisma from "./src/models/database.js";
 
 dotenv.config({quiet: true});
 
+const PORT = process.env.PORT || 3000;
+
+const application = express();
+
+application.listen(PORT, () => {
+    const date = new Date();
+    const localDateTime = date.toLocaleDateString();
+    const timezone = -date.getTimezoneOffset() / 60;
+    const timezoneFormatted = Intl.NumberFormat("en-US", 
+        {signDisplay: "exceptZero"}
+    ).format(timezone);
+
+    console.log(`\x1b[32mApplication running on port ${PORT}.`);
+    console.log(`Process started at: ${localDateTime}, `+
+        `UTC ${timezoneFormatted}:00 GMT\x1b[0m`);
+});
+
+
+function terminateProcess() {
+    prisma.$disconnect();
+    console.log(`\x1b[31m` + 
+        `Process terminated at: ${new Date().toLocaleString()}\x1b[0m`);
+    process.exit(1);
+}
+
+process.on("SIGINT", terminateProcess);
+process.on("SIGTERM", terminateProcess);
