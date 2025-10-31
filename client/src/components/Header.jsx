efab79578127fb809386d8cc80c0f90697687be8
Adrian-rospx <adrianrosu5400@gmail.com>
2025-10-31 19:42:58 +0200

    style: basic header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return(
+        <header>
+            <div id="header-container">
+                <div id="header-main-title">
+                    <h1>Bran Castle</h1>
+                </div>
+            </div>
+        </header>
+    );
+}
